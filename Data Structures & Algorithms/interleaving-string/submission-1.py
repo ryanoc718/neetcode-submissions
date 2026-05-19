240b7205a3c05a407218f76a125174936c9d8eae
Ryan O'Connor <ryanoconnor718@gmail.com>
2026-05-18 21:26:40 -0400

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        memo = {}
+        def dfs(one, two, three):
+            if one >= len(s1) and two >= len(s2):
+                memo[(one, two)] = True
+                return True
+            if three == len(s3):
+                memo[(one, two)] = False
+                return False
+            if (one, two) in memo:
+                return memo[(one, two)]
+            pickOne, pickTwo = False, False
+            if one < len(s1) and s1[one] == s3[three]:
+                pickOne = dfs(one+1, two, three+1)
+            if two < len(s2) and s2[two] == s3[three]:
+                pickTwo = dfs(one, two+1, three+1)
+            memo[(one, two)] = pickOne or pickTwo
+            return memo[(one, two)]
+        return dfs(0,0,0) and len(s1)+len(s2) == len(s3)
